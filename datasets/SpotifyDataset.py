30d2987123be83d4aa352340c02addbec435993c
ahmad0790 <ahmadkhan2013@u.northwestern.edu>
2020-11-23 00:15:45 -0800

    adding data preprocessing and simple Transformer model for pretraining BERT

---
+++ datasets/SpotifyDataset.py
@@ -0,0 +1,127 @@
+import math
+import random
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class SpotifyDataset(Dataset):
+    
+    def __init__(self, tracks, skips, track_vocab=None, bert=False, bert_mask_proportion=0.2):
+        
+        if len(tracks) != len(skips):
+            raise ValueError("Session Tracks and Session Skips have different sizes")
+        
+        self.tracks = tracks
+        self.skips = skips
+        self.PAD_INDEX = track_vocab['pad']
+        self.MASK_INDEX = track_vocab['mask']
+        self.SESSION_HALF_LENGTH = 10
+        self.bert = bert
+        self.bert_mask_proportion = bert_mask_proportion
+        self.len_track_vocab = len(track_vocab)
+
+    def __len__(self):
+        return len(self.tracks)
+    
+    def __getitem__(self, index):
+
+        input_data, labels_data = self.split_session(self.tracks[index], self.skips[index])
+
+        input_sequence, input_skips = input_data
+        label_sequence, label_skips = labels_data
+        seq_len = len(input_sequence)
+
+        # for BERT pretraining we only need to train on the masked train sequence
+        if self.bert==True:
+            input_sequence, label_sequence = self.mask_words(input_sequence)
+            
+            #pad to make all sessions even length
+            padding_tokens = [self.PAD_INDEX for i in range(self.SESSION_HALF_LENGTH - len(label_sequence))]
+            input_sequence.extend(padding_tokens)
+            label_sequence.extend(padding_tokens)
+
+            return input_sequence, label_sequence
+
+        else:
+
+            #pad to make all sessions even length
+            
+            padding_tokens = [self.PAD_INDEX for i in range(self.SESSION_HALF_LENGTH - len(input_sequence))]
+            input_sequence.extend(padding_tokens)
+            input_skips.extend(padding_tokens)
+           
+            padding_tokens = [self.PAD_INDEX for i in range(self.SESSION_HALF_LENGTH - len(label_sequence))] 
+            label_sequence.extend(padding_tokens)
+            label_skips.extend(padding_tokens)
+
+            return input_sequence, input_skips, label_sequence, label_skips
+
+    def split_session(self, sequence, skip_sequence):
+
+        seq_len = math.floor(len(sequence)//2)
+
+        train_sequence = sequence[0:seq_len]
+        test_sequence = sequence[seq_len:]
+        train_skips = skip_sequence[0:seq_len]
+        test_skips = skip_sequence[seq_len:]
+
+        input_data = (train_sequence, train_skips)
+        labels_data = (test_sequence, test_skips)
+
+        return input_data, labels_data
+
+    #only for BERT masking words for pre training
+    def mask_words(self, train_sequence):
+
+        seq_len = len(train_sequence)
+        labels = []
+
+        for i in range(seq_len):
+
+            mask_prob = random.random()
+            track_index = train_sequence[i]
+
+            if mask_prob <= self.bert_mask_proportion:
+                
+                #mask_prob = random.random()
+                mask_prob /= self.bert_mask_proportion
+
+                if mask_prob <= 0.8:
+                    train_sequence[i] = self.MASK_INDEX
+
+                elif mask_prob <= 0.9:
+                    train_sequence[i] = random.randrange(self.len_track_vocab)
+
+                else:
+                    train_sequence[i] = track_index
+
+                labels.append(track_index)
+
+            else:
+
+                #set to 0 any word that is not masked. These will not be trained on in BERT
+                labels.append(0)
+
+        return train_sequence, labels
+
+def custom_collate_fn(batch):
+    input_sequence = [item[0] for item in batch]
+    input_skips = [item[1] for item in batch]
+    label_sequence = [item[2] for item in batch]
+    label_skips = [item[3] for item in batch]
+    
+    input_sequence = torch.LongTensor(input_sequence)
+    input_skips = torch.LongTensor(input_skips)
+    label_sequence = torch.LongTensor(label_sequence)
+    label_skips = torch.LongTensor(label_skips)
+
+    return input_sequence, input_skips, label_sequence, label_skips
+
+def bert_collate_fn(batch):
+
+    input_sequence = [item[0] for item in batch]
+    label_sequence = [item[1] for item in batch]
+    
+    input_sequence = torch.LongTensor(input_sequence)
+    label_sequence = torch.LongTensor(label_sequence)
+
+    return input_sequence, label_sequence
---
+++ utils/data_preprocessing.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+import glob
+import pickle
+
+def generate_vocab(training_path, min_freq, idx_start=0, idx_end = 65):
+    
+    print("GENERATING TRACK VOCAB DICTIONARY")
+    train_input_logs = sorted(glob.glob(training_path + "log_0*.csv"))
+    
+    for i in range(idx_start, idx_end):
+        
+        print(train_input_logs[i])
+        df = pd.read_csv(train_input_logs[i])
+        track_counts = df.groupby("track_id_clean")['track_id_clean'].count()
+        if i == 0:
+            all_track_counts = track_counts.copy()
+        else:
+            all_track_counts = all_track_counts.append(track_counts)
+            all_track_counts = all_track_counts.groupby('track_id_clean').sum()
+
+    all_track_counts = all_track_counts[all_track_counts>=min_freq]
+    
+    track_vocab = {'pad':0, 'mask':1, 'unk':2, 'sos':3, 'eos':4}
+    track_ids = {track_id: i+len(track_vocab) for i, track_id in enumerate(all_track_counts.index)}
+    track_vocab.update(track_ids)
+    
+    return track_vocab
+
+def generate_training_data(training_path, track_vocab, train_input_logs, idx_start=0, idx_end = 65,train=True):
+
+    all_session_tracks = []
+    all_session_skips = []
+    #all_session_lengths = []
+    
+    print("GENERATING DATA")
+    
+    for i in range(idx_start, idx_end):
+        print(train_input_logs[i])
+        df = pd.read_csv(train_input_logs[i])
+        df['skip_2'] = df['skip_2'].astype(int)
+        df['track_id_clean'] = df['track_id_clean'].map(track_vocab).fillna(track_vocab['unk']).astype(int)
+        session_tracks = df.groupby("session_id")['track_id_clean'].apply(list)
+        session_skips = df.groupby("session_id")['skip_2'].apply(list)
+        session_skips = list(session_skips.values)
+        session_tracks = list(session_tracks.values)
+
+        all_session_tracks.extend(session_tracks)
+        all_session_skips.extend(session_skips)
+    
+    return all_session_tracks, all_session_skips
+
+
+def generate_track_features_embedding_weights(track_vocabs):
+        #get embedding data
+        #filter for only relevant embedding
+        #create embedding matrix with row index corresponding to the track id value in the track vocab
+        
+        print("GENERATING TRACK FEATURE EMBEDDINGS")
+        track1 = pd.read_csv("../data/track_features/tf_000000000000.csv")
+        track2 = pd.read_csv("../data/track_features/tf_000000000001.csv")
+        tracks = track1.append(track2, ignore_index=True)
+        tracks_mapping = pd.DataFrame.from_dict(track_vocabs, orient='index', columns = ['track_index']).reset_index()
+        tracks = tracks.merge(tracks_mapping, left_on='track_id', right_on='index').sort_values('track_index')
+        track_embedding = np.zeros((len(track_vocabs), 28))
+        track_embedding[5:,:] = tracks.iloc[0,np.r_[1:16,17:30]].values
+        
+        return track_embedding
+
+
+training_path = '../data/training_set_0/'
+testing_path = '../data/training_set_1/'
+IDX_END = 65
+MIN_FREQ = 10
+
+print("------GENERATING TRAINING DATA------")
+
+train_input_logs = sorted(glob.glob(training_path + "log_0*.csv"))
+track_vocabs  = generate_vocab(training_path, min_freq=MIN_FREQ, idx_start=0, idx_end = IDX_END)
+
+f = open("../data/track_vocabs.pkl","wb")
+pickle.dump(track_vocabs,f)
+f.close()
+
+#track_embedding = generate_track_features_embedding_weights(track_vocabs)
+#np.save('../data/track_embedding.npy', track_embedding)
+
+'''
+all_session_tracks, all_session_skips = generate_training_data(training_path, track_vocabs, train_input_logs, idx_start = 0, idx_end = IDX_END)
+
+
+f = open("../data/all_session_tracks_train.pkl","wb")
+pickle.dump(all_session_tracks, f)
+f.close()
+
+f = open("../data/all_session_skips_train.pkl","wb")
+pickle.dump(all_session_skips, f)
+f.close()
+
+print("------GENERATING TESTING DATA------")
+test_input_logs = sorted(glob.glob(testing_path + "log_1*.csv"))
+all_session_tracks, all_session_skips = generate_training_data(training_path, track_vocabs, test_input_logs, idx_start = 0, idx_end = IDX_END)
+f = open("../data/all_session_tracks_test.pkl","wb")
+pickle.dump(all_session_tracks, f)
+f.close()
+
+f = open("../data/all_session_skips_test.pkl","wb")
+pickle.dump(all_session_skips, f)
+f.close()
+'''
