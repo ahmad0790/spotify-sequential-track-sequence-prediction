fixing BERT masking aliasing bug

datasets/SpotifyDataset.py
@@ -1,4 +1,5 @@
 import math
+import copy
 import random
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -18,6 +19,7 @@ class SpotifyDataset(Dataset):
         #self.MASK_INDEX = 0
         #self.MASK_INDEX  = 1
         self.MASK_INDEX = track_vocab['mask']
+        print(self.MASK_INDEX)
         
         self.SESSION_HALF_LENGTH = 10
         self.bert = bert
@@ -69,7 +71,7 @@ class SpotifyDataset(Dataset):
 
     def split_session(self, sequence, skip_sequence):
 
-        seq_len = math.floor(len(sequence)//2)
+        seq_len = len(sequence)//2
 
         train_sequence = sequence[0:seq_len]
         test_sequence = sequence[seq_len:]
@@ -82,15 +84,16 @@ class SpotifyDataset(Dataset):
         return input_data, labels_data
 
     #only for BERT masking words for pre training
-    def mask_words(self, train_sequence):
+    def mask_words(self, input_sequence):
 
-        seq_len = len(train_sequence)
+        masked_sequence = copy.deepcopy(input_sequence)
+        #masked_sequence = []
         labels = []
 
-        for i in range(seq_len):
+        for i in range(len(input_sequence)):
 
             mask_prob = random.random()
-            track_index = train_sequence[i]
+            #track_index = train_sequence[i]
 
             if mask_prob <= self.bert_mask_proportion:
                 
@@ -98,22 +101,22 @@ class SpotifyDataset(Dataset):
                 mask_prob /= self.bert_mask_proportion
 
                 if mask_prob <= 0.8:
-                    train_sequence[i] = self.MASK_INDEX
+                    masked_sequence[i] = self.MASK_INDEX
 
                 elif mask_prob <= 0.9:
-                    train_sequence[i] = random.randrange(self.len_track_vocab)
+                    masked_sequence[i] = random.randrange(2,self.len_track_vocab)
 
                 else:
-                    train_sequence[i] = track_index
+                    masked_sequence[i] = input_sequence[i]
 
-                labels.append(track_index)
+                labels.append(input_sequence[i])
 
             else:
-
                 #set to 0 any word that is not masked. These will not be trained on in BERT
+                masked_sequence[i] = input_sequence[i]
                 labels.append(0)
 
-        return train_sequence, labels
+        return masked_sequence, labels
 
 def custom_collate_fn(batch):
     input_sequence = torch.LongTensor([item[0] for item in batch])

train_bert.py
@@ -74,10 +74,8 @@ def accuracy(output, target):
     """Computes the average accuracy of the predicted skip sequence"""
     
     seq_len = target.shape[1]
-    output = output[target>0]
-    target = target[target>0]
     correct = output.eq(target)
-    correct = correct.sum() * 1.0
+    correct = correct.sum(dim=1) * 1.0
     acc = correct / seq_len
     return acc
 
@@ -145,10 +143,8 @@ def train_bert(model, dataloader, optimizer, criterion, scheduler = None, device
         if batch_idx % 100 ==0:
             print("Batch: %d, Train Loss: %.4f, Train Accuracy: %.4f" % ((batch_idx+1), avg_loss.avg, avg_acc.avg))
 
-        '''
         if (batch_idx+1) % 1000 ==0:
             break
-        '''
 
     return avg_loss.avg, avg_acc.avg
 
@@ -234,7 +230,6 @@ BATCH_SIZE = 128
 MAX_LEN = 20
 EPOCHS = 5
 SKIP = False
-PAD_MASK = 0
 bert_masking_prob=0.4
 
 train_dataset = SpotifyDataset(train_tracks, train_skips, track_vocab, bert=True, bert_mask_proportion = bert_masking_prob, skip_pred=SKIP, padding=False)